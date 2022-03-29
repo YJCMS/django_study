326357a19da84242335daa265d84d538bb30f843
이승현 <sen9563@gmail.com>
2022-03-29 23:44:55 +0900

    폼

--- pybo/urls.py
+++ pybo/urls.py
@@ -6,5 +6,6 @@ app_name = 'pybo'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
-    path('answer/creat/<int:question_id>/', views.answer_create, name='answer_create')
+    path('answer/creat/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('question/create/', views.question_create, name ='question_create'),
 ]
