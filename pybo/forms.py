07d3e483557004ff2bcde9956a4c5e96f67c26c5
이승현 <sen9563@gmail.com>
2022-03-30 02:05:48 +0900

    django 기본 요소 공부

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,8 +1,27 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['subject', 'content']
+
+    # question_form.html에 {{ form.as_p }}를 사용해 자동으로 폼을 생성했을때, 화면 디자인을 바꿀때 사용
+        # widgets = {
+        #     'subject': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'content': forms.Textarea(attrs={'class': 'form-control', 'rows' : 10}),
+        # }
+
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
